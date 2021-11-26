8b6337434edbb689b20d35ae3841b99722d22b8b
Jakub Chábek <jakub@chabek.cz>
2021-11-26 20:55:43 +0100

    Add more config options and improve documentation

--- panicwatch.go
+++ panicwatch.go
@@ -1,3 +1,7 @@
+// Package panicwatch guarantees you that you will never miss a panic. Use it to reliably log any unhandled panics
+// that may occur in your application. This is completely transparent to your application, and it doesn't affect
+// it in any way. All signal handling and file descriptor manipulation (either from inside or outside) is still under
+// your control.
 package panicwatch
 
 import (
@@ -14,11 +18,15 @@ import (
 	goerrors "github.com/go-errors/errors"
 )
 
+// Panic holds information about a panic parsed from stderr of your application.
 type Panic struct {
 	Message string
 	Stack   string
 }
 
+// AsError returns this structure as instance of error interface. When the panic message and stack aren't malformed,
+// it will return *goerrors.Error, otherwise it will fall back to a simple *errors.errorString,
+// containing just the message.
 func (p Panic) AsError() error {
 	parsedErr, err := goerrors.ParsePanic("panic: " + p.Message + "\n" + p.Stack)
 	if err != nil {
@@ -28,10 +36,20 @@ func (p Panic) AsError() error {
 	return parsedErr
 }
 
+// Config hold the configuration of panicwatch.
 type Config struct {
-	OnPanic        func(Panic) // required
-	OnWatcherError func(error) // optional, used for reporting watcher process errors
-	OnWatcherDied  func(error) // optional, you should provide it to shutdown your application gracefully
+	// BufferSize specifies the size of the read buffer between dup-ed stderr and the real one. Optional/
+	BufferSize int
+	// PanicDetectorBufferSize specifies the size of the buffer used to detect panic.
+	// Too low value will cause the detection to fail. Optional.
+	PanicDetectorBufferSize int
+	// OnPanic is a callback that will be called after your application dies, if a panic is detected. Required.
+	OnPanic func(Panic)
+	// OnWatcherErr is a callback that will be called when watcher process encounters an error. Optional.
+	OnWatcherError func(error)
+	// OnWatcherDied is a callback that will be called when watcher process dies.
+	// It is recommended to set this callback to shut down your application gracefully. Optional.
+	OnWatcherDied func(error)
 }
 
 const (
@@ -39,9 +57,13 @@ const (
 	cookieValue = "zQXfl15CShjg5yQzEqoGAIgFeyXhlr9JQABuYCXm"
 )
 
+// Start validates panicwatch config, replaces the stderr file descriptor with a new one and starts a watcher process.
+// This watcher process will read the original stderr and tee it into the replaced file descriptor. When the application
+// exits, the watcher process will check if there was a panic in the original stderr. If yes, it will call the OnPanic
+// callback. If the watcher process encounters an error or dies, then appropriate callback is called if configured.
 func Start(config Config) error {
-	if config.OnPanic == nil {
-		return errors.New("OnPanic callback must be set")
+	if err := checkConfig(&config); err != nil {
+		return err
 	}
 
 	if os.Getenv(cookieName) == cookieValue {
@@ -95,11 +117,35 @@ func Start(config Config) error {
 	return nil
 }
 
+func checkConfig(config *Config) error {
+	if config.OnPanic == nil {
+		return errors.New("OnPanic callback must be set")
+	}
+
+	if config.BufferSize < 0 {
+		return errors.New("BufferSize can't be less than zero")
+	}
+
+	if config.BufferSize == 0 {
+		config.BufferSize = 1e5
+	}
+
+	if config.PanicDetectorBufferSize < 0 {
+		return errors.New("PanicDetectorBufferSize can't be less than zero")
+	}
+
+	if config.PanicDetectorBufferSize == 0 {
+		config.PanicDetectorBufferSize = 1e5
+	}
+
+	return nil
+}
+
 func runMonitoringProcess(config Config) {
 	signal.Ignore()
 
-	readBuffer := make([]byte, 1000)
-	buffer := rbuf.NewFixedSizeRingBuf(1e5)
+	readBuffer := make([]byte, config.BufferSize)
+	buffer := rbuf.NewFixedSizeRingBuf(config.PanicDetectorBufferSize)
 	reader := io.TeeReader(os.Stdin, os.Stdout)
 
 	for {
